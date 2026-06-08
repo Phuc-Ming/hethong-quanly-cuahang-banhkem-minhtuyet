fe302ab226c592f1bee4cb8ce307d1fdff6422b9
Phuc-Ming <152255899+Phuc-Ming@users.noreply.github.com>
2026-06-08 13:34:31 +0700

    Delete src/backend directory

--- src/backend/wwwroot/js/xu-ly.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('input[type="number"]').forEach(function (input) {
-        input.addEventListener('input', function () {
-            if (Number(input.value) < 0) input.value = 0;
-        });
-    });
-});
